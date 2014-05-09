a6cee32f4c102fd2459cb3134b75534b0937bd19
Thomas RICHARD <thomas.richard.pro@gmail.com>
2014-05-09 02:17:12 +0200

    Moved IdleState initialization logic to its constructor.

--- src/oftp/automaton/state/IdleState.java
+++ src/oftp/automaton/state/IdleState.java
@@ -1,12 +1,33 @@
 package oftp.automaton.state;
 
+import automaton.action.Action;
 import automaton.state.StateImpl;
+import automaton.transition.Transition;
+import oftp.automaton.OFTPAutomaton;
+import oftp.automaton.action.InitSocketAction;
+import oftp.automaton.action.SendSSRMAction;
+import oftp.automaton.event.monitor.FConnectRequestArchetype;
+import oftp.automaton.event.monitor.NetworkConnectionIndicationArchetype;
 
 
 public class IdleState extends StateImpl {
 
-	public IdleState() {
+	public IdleState(OFTPAutomaton oftp) {
 		super("IDLE");
-//		addTranstion();
+
+		Action initSocketAction = new InitSocketAction(oftp);
+		Action sendSSRMAction = new SendSSRMAction(oftp);
+
+		Transition fConReqIWfRmTransition = new Transition();
+		fConReqIWfRmTransition.addAction(initSocketAction);
+		fConReqIWfRmTransition.setNextState(new InitiatorWaitingForReadyMessageState());
+
+		Transition nConIndANcOnlyTransition = new Transition();
+		nConIndANcOnlyTransition.addAction(initSocketAction);
+		nConIndANcOnlyTransition.addAction(sendSSRMAction);
+		nConIndANcOnlyTransition.setNextState(new AcceptorNetworkConnectionOnlyState());
+
+		addTranstion(new FConnectRequestArchetype(), fConReqIWfRmTransition);
+		addTranstion(new NetworkConnectionIndicationArchetype(), nConIndANcOnlyTransition);
 	}
 }
