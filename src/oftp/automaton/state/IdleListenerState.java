093c0b2f4baefb8be109d50e1a3f8c830294f430
Pierre BINAULD <pierre.binauld+github@gmail.com>
2014-05-13 18:38:58 +0200

    Implementation of IDLELI state

--- src/oftp/automaton/state/IdleListenerState.java
+++ src/oftp/automaton/state/IdleListenerState.java
@@ -1,6 +1,13 @@
 package oftp.automaton.state;
 
+import oftp.automaton.AbortOrigin;
+import oftp.automaton.EndSessionAnswerReason;
 import oftp.automaton.OftpAutomaton;
+import oftp.automaton.action.CreateFAbortIndicationAction;
+import oftp.automaton.archetype.network.StartFileArchetype;
+import oftp.automaton.predicate.idleli.InvalidStartFileCommandPredicate;
+import automaton.predicate.Predicate;
+import automaton.transition.Transition;
 
 
 public class IdleListenerState extends OftpAbstractState {
@@ -9,5 +16,16 @@ public class IdleListenerState extends OftpAbstractState {
 	
 	public IdleListenerState(OftpAutomaton oftp) {
 		super(oftp, NAME);
+		
+		Predicate p1 = new InvalidStartFileCommandPredicate(oftp);
+		
+		Transition a = new Transition()
+			.setPredicate(p1)
+			.addAction(true, new CreateFAbortIndicationAction(oftp, EndSessionAnswerReason.INVALID_DATA, AbortOrigin.LOCAL))
+			.setNextState(true, oftp.getIdleState())
+			.addAction(false, new CreateStartFileIndicationAction(oftp))
+			.setNextState(false, new OpenInputPendingState(oftp));
+		
+		addTranstion(new StartFileArchetype(), a);
 	}
 }
